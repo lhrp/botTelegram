be8b340f2e46c3494257c95682d4802f1eab94db
Leonardo H R Paulino <leonardo@lhrp.com.br>
2023-11-26 21:47:31 -0300

    Ok

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import telebot, requests, os
+import telebot, requests, os, pymysql
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
@@ -6,6 +6,19 @@ load_dotenv(find_dotenv())
 CHAVE_API = os.environ.get("CHAVE_API")
 bot = telebot.TeleBot(CHAVE_API)
 
+
+# Configurações do banco de dados
+db_config = {
+    'host': os.environ.get("MYSQL_DATABASE_HOST"),
+    'user': os.environ.get("MYSQL_DATABASE_USER"),
+    'password': os.environ.get("MYSQL_DATABASE_PASSWORD"),
+    'db': os.environ.get("MYSQL_DATABASE_DB"),
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+# Função para criar uma conexão com o banco de dados
+def create_db_connection():
+  return pymysql.connect(**db_config)
 
 ###########################################################################
 @bot.message_handler(commands=["1"])
@@ -40,11 +53,12 @@ def verificar(mensagem):
 def responder(mensagem):
   texto = """
     Escolha uma opção para continuar (Clique no item):
-     Linkedin (/1 ou /linkedin)
+ww     Linkedin (/1 ou /linkedin)
      Twitch (/2 ou /twitch)
      YouTube (/3 ou /youtube)
-     Github (/4 ou /githun)
+     Github (/4 ou /github)
 Qualquer outro comando diferente dos mencionados acima não irá funcionar!!!"""
+  
   bot.reply_to(mensagem, texto)
 
 bot.polling()
